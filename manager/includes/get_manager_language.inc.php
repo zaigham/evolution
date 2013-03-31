7146bd78f303aa99940691330537dbe6eb53b505
TimGS <timspe@gmail.com>
2013-03-31 00:18:00 +0000

    Ensure both $_lang and $_country_lang encodings are converted if manager language is not UTF8

--- manager/includes/get_manager_language.inc.php
+++ manager/includes/get_manager_language.inc.php
@@ -20,21 +20,7 @@ if ($manager_language != 'english') {
 }
 
 // Convert $_lang to modx_charset
-if (isset($modx->config['modx_charset']) &&  $modx->config['modx_charset'] != 'UTF-8') {
-    foreach($_lang as $__k => $__v) {
-        $tmp = iconv('UTF-8', $modx->config['modx_charset'].'//TRANSLIT', $_lang[$__k]);
-        if ($_lang[$__k] == iconv($modx->config['modx_charset'], 'UTF-8//TRANSLIT', $tmp)) {
-            // No errors - conversion possible from UTF-8 to selected encoding
-            $_lang[$__k] = $tmp;
-        } else {
-            // Errors - language file cannot be converted to selected encoding.
-            // Fallback to English as it can be shown in most character encodings, and thus minimises the risk of an unusable manager.
-            $_lang = array();
-            require('lang/english.inc.php');
-            break;
-        }
-    }
-}
+convert_language_array($_lang, 'lang/english.inc.php', '_lang');
 
 // Make manager charset match site charset to avoid editing issues
 $modx_manager_charset = $modx->config['modx_charset'];
@@ -51,6 +37,30 @@ function get_manager_countries($manager_language) {
 	if ($manager_language != 'english') {
 		require('lang/country/'.$manager_language.'_country.inc.php');
 	}
+	
+	convert_language_array($_country_lang, 'lang/country/english_country.inc.php', '_country_lang');
+	
 	return $_country_lang;
+}
+
+// Convert encoding of language array
+function convert_language_array(&$lang, $fallback, $fallback_var) {
+	global $modx;
+	if (isset($modx->config['modx_charset']) &&  $modx->config['modx_charset'] != 'UTF-8') {
+		foreach($lang as $__k => $__v) {
+		    $tmp = iconv('UTF-8', $modx->config['modx_charset'].'//TRANSLIT', $lang[$__k]);
+		    if ($lang[$__k] == iconv($modx->config['modx_charset'], 'UTF-8//TRANSLIT', $tmp)) {
+		        // No errors - conversion possible from UTF-8 to selected encoding
+		        $lang[$__k] = $tmp;
+		    } else {
+		        // Errors - language file cannot be converted to selected encoding.
+		        // Fallback to English as it can be shown in most character encodings, and thus minimises the risk of an unusable manager.
+		        $lang = array();
+		        require($fallback);
+		        $lang = $$fallback_var;
+		        break;
+		    }
+		}
+	}
 }
 
