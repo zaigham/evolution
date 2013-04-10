6bf23bde9d7cb66ec07066bd00886b8caf1fc5df
TimGS <timspe@gmail.com>
2013-04-10 22:31:05 +0100

    Ensure kcfinder uses manager user's language

--- manager/media/browser/kcfinder/clipper_integration.inc.php
+++ manager/media/browser/kcfinder/clipper_integration.inc.php
@@ -13,13 +13,17 @@ if(!isset($_SESSION['mgrValidated'])) {
         exit();
 }
 
-// USE CLIPPERCMS MANAGER LANGUAGE
+// CLIPPERCMS SETTINGS
 if (!defined('IN_MANAGER_MODE')) define('IN_MANAGER_MODE', 'true');
 require_once('../../../includes/document.parser.class.inc.php');
 $modx = new DocumentParser;
 $modx->getSettings();
-require_once('../../../includes/get_manager_language.inc.php');
+$modx->getUserSettings();
+$settings = &$modx->config;
+extract($settings, EXTR_OVERWRITE);
 
+// USE CLIPPERCMS MANAGER LANGUAGE
+require_once('../../../includes/get_manager_language.inc.php');
 if (isset($modx_lang_attribute) && ctype_alpha($modx_lang_attribute) && strlen($modx_lang_attribute) == 2) {
 	$_GET['langCode'] = $_REQUEST['langCode'] = $modx_lang_attribute;
 }
