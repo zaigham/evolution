30fe25ac24c5f3991e0238e5054106480395e702
Keith Penton <kp52@o2.co.uk>
2013-06-17 15:35:40 +0100

    Change $ to jQuery to work in noConflict mode

--- assets/templates/clipperdemo/js/settings.js
+++ assets/templates/clipperdemo/js/settings.js
@@ -1,8 +1,8 @@
 /* [jQuery] */
-$(document).ready(function(){
+jQuery(document).ready(function(){
 
 	/* [Slideshow] */
-	$("#slideshow ul").responsiveSlides({
+	jQuery("#slideshow ul").responsiveSlides({
 		auto: true,             // Boolean: Animate automatically, true or false
 		speed: 500,             // Integer: Speed of the transition, in milliseconds
 		timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
@@ -23,17 +23,17 @@ $(document).ready(function(){
 	/* [/End Slideshow] */	
 	
 	/* [Hide Alert (see Foundation docs)] */
-	$('.alert-box .close').click( function() {
-		$(this).parent().fadeOut(400);
+	jQuery('.alert-box .close').click( function() {
+		jQuery(this).parent().fadeOut(400);
 		return false;
 	});		
 	/* [/End Hide Alert (see Foundation docs)] */
 	
 	/* [jQuery Form Validation] */
-    $('#contactform').isHappy({
+    jQuery('#contactform').isHappy({
 		fields: {
             // reference the field you're talking about, probably by `id`
-            // but you could certainly do $('[name=name]') as well.
+            // but you could certainly do jQuery('[name=name]') as well.
             '.required-name': {
 				required: true,
 				message: 'Please enter your name'
@@ -56,46 +56,46 @@ $(document).ready(function(){
 	/* [/End jQuery Form Validation] */		
 	
 	// Add Class "has-children" if parent "li" has children "ul"
-	$("#header-menu nav > ul > li:has(ul)").addClass('has-children');
+	jQuery("#header-menu nav > ul > li:has(ul)").addClass('has-children');
 	// Add span to "a" if parent "li" has children "ul", this will be used to build
 	// a button to expand the menu when viewing the website on Mobiles phones
-	$("#header-menu nav > ul > li:has(ul) > a").append('<span></span>');	
+	jQuery("#header-menu nav > ul > li:has(ul) > a").append('<span></span>');
 	
 	/* [Media Queries] */
 	enquire.register("only screen and (min-width: 0) and (max-width: 799px)", {
 		setup : function() {
 			// Show/Hide Mobile Menu	
-			$('#header-menu-mobile').click(function () {
-				$('#header-menu-mobile,#header-menu nav > ul').toggleClass('is-open');
+			jQuery('#header-menu-mobile').click(function () {
+				jQuery('#header-menu-mobile,#header-menu nav > ul').toggleClass('is-open');
             });	
 			// Show/Hide Mobile Search
-			$('#header-search-mobile').click(function () { $('#header-search-form,#header-search-mobile,#header-search').toggleClass('is-open'); });
+			jQuery('#header-search-mobile').click(function () { jQuery('#header-search-form,#header-search-mobile,#header-search').toggleClass('is-open'); });
 		},
 		match : function() {
 			// Slide Down/Up the secondary nav for mobile
-			$('#header-menu nav > ul > li.has-children > a > span').click(function(e) {
+			jQuery('#header-menu nav > ul > li.has-children > a > span').click(function(e) {
 				e.preventDefault();
-				$(this).parent().parent().children('ul').slideToggle('fast');
-				$(this).toggleClass('is-open');
+				jQuery(this).parent().parent().children('ul').slideToggle('fast');
+				jQuery(this).toggleClass('is-open');
 			});			
 		},
 		unmatch : function() {
 			// Disable Slide Down/Up if you exit Mobile view
-			$('#header-menu nav > ul > li.has-children > a > span').unbind();
+			jQuery('#header-menu nav > ul > li.has-children > a > span').unbind();
 		}  
 	}).register("only screen and (min-width: 800px)", {
 		match : function() {
 			// Dropdown Menu (delay)
-			$("#header-menu nav > ul > li").hover(function () { 
-				$(this).addClass('hovering'); 
+			jQuery("#header-menu nav > ul > li").hover(function () {
+				jQuery(this).addClass('hovering');
 			},
 			function () { 
-				$(this).removeClass('hovering');
+				jQuery(this).removeClass('hovering');
 			});			
 		},
 		unmatch : function() {
 			// Disable dropdown menu (for mobile)
-			$("#header-menu nav > ul > li").unbind('mouseenter mouseleave');
+			jQuery("#header-menu nav > ul > li").unbind('mouseenter mouseleave');
 		} 
     });
 	/* [/End Media Queries] */
