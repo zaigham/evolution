0af72b99271de9e992f94c552633b37fdfb3df17
TimGS <timspe@gmail.com>
2013-06-21 23:50:55 +0100

    Move jquery noconflict call to be always after the jquery core file. If jquery core file is set in the templating, not the API, this option now has no effect i.e. the templating will take precedence..

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -3249,9 +3249,6 @@ class DocumentParser extends Core {
     	
     	if (!$jquery_included) {	
  			$this->regClientStartupScript($this->config['jquery_url'], array('jquery'=>true));
- 			if ($this->config['jquery_noconflict']) {
- 				$this->regClientStartupScript('<script type="text/javascript">jQuery.noConflict()</script>');
- 			}
  			$jquery_included = true;
  		}
     }
@@ -3372,6 +3369,9 @@ class DocumentParser extends Core {
                 // First entry must be the core - look for any version of jquery before adding another
                 if (!preg_match('/jquery(-\d+\.\d+(\.\d+)?)?(\.min)?\.js/i', $head)) {
                     $output .= $this->jquery_scripts[1]."\n";
+                    if ($this->config['jquery_noconflict']) {
+                        $output .= "<script type=\"text/javascript\">jQuery.noConflict()</script>\n";
+                    }
                 }
                 for($i = 2; $i <= sizeof($this->jquery_scripts); ++$i) {
                     // Further entries must be plugins - look for filename, minified or otherwise.
