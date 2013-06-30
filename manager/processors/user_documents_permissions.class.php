5aee44c4f678aaf18cce86e7021f2e35000d611c
TimGS <timspe@gmail.com>
2013-06-30 18:21:40 +0100

    Add dummy user_documents_permissions.class.php in manager/processors/ for backwards compatability.

---
+++ manager/processors/user_documents_permissions.class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Use of this file is deprecated.
+ * Please use the file included below in manager/includes.
+ */
+require_once(dirname(__FILE__).'/../includes/user_documents_permissions.class.php');
+?>
