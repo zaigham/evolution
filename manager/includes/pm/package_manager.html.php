159d12e04d7e3af710a10d0b1f25682c7719acc1
TimGS <timspe@gmail.com>
2013-07-07 11:51:38 +0100

    Move text from package_manager.html.php to language file

--- manager/includes/pm/package_manager.html.php
+++ manager/includes/pm/package_manager.html.php
@@ -1,24 +1,24 @@
 <?php
 $pkg_manager_html['header'] =
-'<div class="sectionHeader">Package Manager</div>
+'<div class="sectionHeader">'.$_lang['package_manager'].'</div>
 <div class="sectionBody">';
 	
 $pkg_manager_html['form'] =
 '<form action="'.$self_href.'" method="post" style="margin: 20px 0" enctype="multipart/form-data">
 	<fieldset>
-		<label for="pkg_url">Upload a package by manually entering the URL</label>
+		<label for="pkg_url">'.$_lang['package_manager_upload_byurl_label'].'</label>
 		<input type="text" name="pkg_url" id="pkg_url" value="" />
 	</fieldset>
 	<fieldset>
-		<label for="pkg_file">Manually upload a package file from your local machine</label>
+		<label for="pkg_file">'.$_lang['package_manager_upload_byfile_label'].'</label>
 		<input type="file" name="pkg_file" id="pkg_file">
 	</fieldset>
 	<fieldset>
-		<label for="pkg_folder">Manually specify a package folder on the web server</label>
+		<label for="pkg_folder">'.$_lang['package_manager_upload_byfolder_label'].'</label>
 		<input type="text" name="pkg_folder" id="pkg_folder">
 	</fieldset>
 	<fieldset class="submit">
-		<input type="submit" name="go" value="Upload" />
+		<input type="submit" name="go" value="'.$_lang['package_manager_upload'].'" />
 	</fieldset>
 </form>';
 
@@ -26,14 +26,14 @@ $pkg_manager_html['retry_file_form'] =
 '<form action="'.$self_href.'" method="post" style="margin: 20px 0" enctype="multipart/form-data">
 	<fieldset class="submit">
 	    <input type="hidden" name="retry_file" value="1" />
-		<input type="submit" name="go" value="Retry" />
+		<input type="submit" name="go" value="'.$_lang['package_manager_retry'].'" />
 	</fieldset>
 </form>';
 
 $pkg_manager_html['confirm_form'] =
 '<form action="'.$self_href.'" method="post" style="margin: 20px 0">
 	<fieldset class="submit">
-		<input type="submit" name="go" value="Install" />
+		<input type="submit" name="go" value="'.$_lang['package_manager_install'].'" />
 	</fieldset>
 </form>';
 
