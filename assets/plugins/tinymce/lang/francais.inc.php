6fe9743e5d14692bf32d6f37d24dc9575818de63
TimGS <timspe@gmail.com>
2013-10-14 19:40:04 +0100

    Ensure all TinyMCE language files are UTF-8

--- assets/plugins/tinymce/lang/francais.inc.php
+++ assets/plugins/tinymce/lang/francais.inc.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * Filename:       assets/plugins/tinymce/lang/francais.inc.php
+ * Filename:       assets/plugins/tinymce/lang/francais-utf8.inc.php
  * Function:       French language file for TinyMCE
- * Encoding:       ISO
+ * Encoding:       UTF-8
  * Author:         French community
- * Date:           2007/04/13
+ * Date:           2007/04/13, corrected on 28/07/09
  * Version:        2.1.0
- * MODx version:   0.9.6
+ * MODx version:   Evolution 1.0
 */
 
 include_once(dirname(__FILE__).'/english.inc.php'); // fallback for missing defaults or new additions
 
-$_lang['tinymce_editor_theme_title'] = "ThËme:";
-$_lang['tinymce_editor_theme_message'] = "Vous pouvez sÈlectionner quel thËme ou template utiliser avec la barre d'outils TinyMCE.";
-$_lang['tinymce_editor_custom_plugins_title'] = "Plugins :";
-$_lang['tinymce_editor_custom_plugins_message'] = "Indiquez les plugins ‡ utiliser pour le thËme 'personnalisÈ', en les sÈparant par une virgule.";
-$_lang['tinymce_editor_custom_buttons_title'] = "Boutons :";
-$_lang['tinymce_editor_custom_buttons_message'] = "Indiquez les boutons ‡ utiliser pour le thËme 'personnalisÈ', en les sÈparant par une virgule. Chaque champ correspond ‡ une ligne dans la barre d'outils. Assurez-vous que pour chacun des boutons sÈlectionnÈs, le plugin correspondant est indiquÈ dans le champ de saisie 'Plugins'.";
-$_lang["tinymce_editor_css_selectors_title"] = "SÈlecteurs CSS:";
-$_lang["tinymce_editor_css_selectors_message"] = "Vous pouvez spÈcifier une liste de sÈlecteurs disponibles depuis la barre d'outils. DÈfinissez-les de la maniËre suivante :<br /> 'Nom de la classe 1=class1;Nom de la classe 2=class2'<br />Prenons l'exemple de la classe <b>.mono</b> et <b>.smallText</b> dans votre feuille de style. Vous pouvez les appeler de la faÁon suivante : <br />'Monospaced text=mono;Small text=smallText'<br />La derniËre entrÈe de la ligne ne doit pas Ítre suivie du point-virgule ( ; ).";
-$_lang['tinymce_editor_relative_urls_title'] = "Chemin d'accËs aux fichiers:";
-$_lang['tinymce_editor_relative_urls_message'] = "Cette option vous permet de dÈfinir comment gÈrer les chemins d'accËs pour les liens internes. Note: Document relative links may not work right with friendly alias paths. Also, links might need to be changed if you move your site to a different domain or subdirectory if links are root relative or full path.";
+$_lang['tinymce_editor_theme_title'] = "Th√®me:";
+$_lang['tinymce_editor_theme_message'] = "Vous pouvez s√©lectionner quel th√®me ou template utiliser avec la barre d'outils TinyMCE.";
+$_lang['tinymce_editor_custom_plugins_title'] = "Plugins:";
+$_lang['tinymce_editor_custom_plugins_message'] = "Indiquez les Plugins √† utiliser pour le th√®me 'personnalis√©', en les s√©parant par une virgule.";
+$_lang['tinymce_editor_custom_buttons_title'] = "Boutons:";
+$_lang['tinymce_editor_custom_buttons_message'] = "Indiquez les boutons √† utiliser pour le th√®me 'personnalis√©', en les s√©parant par une virgule. Chaque champ correspond √† une ligne dans la barre d'outils. Assurez-vous que pour chacun des boutons s√©lectionn√©s, le plugin correspondant est indiqu√© dans le champ de saisie 'Plugins'.";
+$_lang["tinymce_editor_css_selectors_title"] = "S√©lecteurs CSS:";
+$_lang["tinymce_editor_css_selectors_message"] = "Vous pouvez sp√©cifier une liste de s√©lecteurs disponibles depuis la barre d'outils. D√©finissez-les de la mani√®re suivante :<br /> 'Nom de la classe 1=class1;Nom de la classe 2=class2'<br />Prenons l'exemple de la classe <b>.mono</b> et <b>.smallText</b> dans votre feuille de style. Vous pouvez les appeler de la fa√ßon suivante: <br />'Monospaced text=mono;Small text=smallText'<br />La derni√®re entr√©e de la ligne ne doit pas √™tre suivie du point-virgule ( ; ).";
+$_lang['tinymce_editor_relative_urls_title'] = "Chemin d'acc√®s aux fichiers:";
+$_lang['tinymce_editor_relative_urls_message'] = "Cette option vous permet de d√©finir comment g√©rer les chemins d'acc√®s pour les liens internes. NOTE: Les liens relatifs peuvent ne pas fonctionner correctement avec les alias simples. De plus, si vos liens sont relatifs √† la racine du site ou d√©crivent un chemin complet, vous devrez peut-√™tre les modifier en cas de d√©placement de votre site sur un nom de domaine diff√©rent.";
 $_lang["tinymce_compressor_title"] = "Compression:";
-$_lang["tinymce_compressor_message"] = "Cette option active/dÈsactive la compression Gzip de TinyMCE afin de rÈduire le temps de chargement de la barre d'outils. Si votre serveur ne supporte pas la compression Gzip, laissez cette option sur disable.";
+$_lang["tinymce_compressor_message"] = "Cette option active/d√©sactive la compression Gzip de TinyMCE afin de r√©duire le temps de chargement de la barre d'outils. Si votre serveur ne supporte pas la compression Gzip, laissez cette option sur disable.";
 $_lang['tinymce_settings'] = "Configuration de TinyMCE";
 $_lang['tinymce_theme_simple'] = "Simple";
-$_lang['tinymce_theme_advanced'] = "AvancÈ";
+$_lang['tinymce_theme_advanced'] = "Avanc√©";
 $_lang['tinymce_theme_editor'] = "Content Editor";
-$_lang['tinymce_theme_custom'] = "PersonnalisÈ";
-$_lang['tinymce_theme_global_settings'] = "Utilisez le paramËtre global";
+$_lang['tinymce_theme_custom'] = "Personnalis√©";
+$_lang['tinymce_theme_global_settings'] = "Utilisez le param√®tre global";
 ?>
--- assets/plugins/tinymce/lang/spanish.inc.php
+++ assets/plugins/tinymce/lang/spanish.inc.php
@@ -10,13 +10,13 @@
 */
 
 $_lang['tinymce_editor_theme_title'] = "Tema:";
-$_lang['tinymce_editor_theme_message'] = "Aquí puede seleccionar el tema o piel a usar con el editor.";
+$_lang['tinymce_editor_theme_message'] = "AquÃ­ puede seleccionar el tema o piel a usar con el editor.";
 $_lang['tinymce_editor_custom_plugins_title'] = "Plugins personalizados:";
 $_lang['tinymce_editor_custom_plugins_message'] = "Ingrese los plugins a usar para el tema 'personalizado' en un listado separado por comas.";
 $_lang['tinymce_editor_custom_buttons_title'] = "Botones personalizados:";
-$_lang['tinymce_editor_custom_buttons_message'] = "Ingrese los botones a usar en el tema 'personalizado' en un listado separado por comas para cada hilera. Asegurese de que cada botón tiene el plugin relacionado habilitado en la configuración de 'Plugins personalizados'.";
+$_lang['tinymce_editor_custom_buttons_message'] = "Ingrese los botones a usar en el tema 'personalizado' en un listado separado por comas para cada hilera. Asegurese de que cada botÃ³n tiene el plugin relacionado habilitado en la configuraciÃ³n de 'Plugins personalizados'.";
 $_lang['tinymce_editor_css_selectors_title'] = "Selectores CSS:";
-$_lang['tinymce_editor_css_selectors_message'] = "Aquí puede ingresar un listado de los selectores que deben estar disponibles en el editor. Ingreselos como sigue:<br />'displayName=selectorName;displayName2=selectorName2'<br />Por ejemplo, imagine que tiene los selectores <b>.mono</b> and <b>.smallText</b> en su archivo CSS, puede agregarlos aquí como:<br />'Monospaced text=mono;Small text=smallText'<br />Note que la última entrada no debe tener el punto y coma luego de ella.";
+$_lang['tinymce_editor_css_selectors_message'] = "AquÃ­ puede ingresar un listado de los selectores que deben estar disponibles en el editor. Ingreselos como sigue:<br />'displayName=selectorName;displayName2=selectorName2'<br />Por ejemplo, imagine que tiene los selectores <b>.mono</b> and <b>.smallText</b> en su archivo CSS, puede agregarlos aquÃ­ como:<br />'Monospaced text=mono;Small text=smallText'<br />Note que la Ãºltima entrada no debe tener el punto y coma luego de ella.";
 $_lang['tinymce_settings'] = "Configuraciones TinyMCE";
 $_lang['tinymce_theme_simple'] = "Simple";
 $_lang['tinymce_theme_advanced'] = "Advanzado";
@@ -24,5 +24,5 @@ $_lang['tinymce_theme_editor'] = "Editor de contenido";
 $_lang['tinymce_theme_custom'] = "Personalizado";
 $_lang['tinymce_theme_creative'] = 'Creative';
 $_lang['tinymce_theme_logic'] = 'xhtml';
-$_lang['tinymce_theme_global_settings'] = "Use la configuración global";
+$_lang['tinymce_theme_global_settings'] = "Use la configuraciÃ³n global";
 ?>
